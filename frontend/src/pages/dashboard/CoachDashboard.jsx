8f43663eade87834d477668d92bd80191014c154
Subhasish Panda <subhasish@Subhasishs-MacBook-Pro.local>
2025-11-29 21:52:47 +0530

    Made changes

--- frontend/src/pages/dashboard/CoachDashboard.jsx
+++ frontend/src/pages/dashboard/CoachDashboard.jsx
@@ -1319,7 +1319,7 @@ const CoachDashboard = () => {
                               </>
                             )}
 
-                            {(event.currentParticipants || 0) > 0(
+                            {(event.currentParticipants || 0) > 0 &&
                               <div
                                 onClick={() => handleEventPayment(event.id)}
                                 className="bg-orange-600 text-white px-4 py-2 rounded-lg text-sm hover:bg-orange-700 transition-colors inline-flex items-center cursor-pointer"
@@ -1329,8 +1329,8 @@ const CoachDashboard = () => {
                                 </svg>
                                 Payment
                               </div>
-                            )}
-                           {console.log(event)};
+                            }
+                            {console.log(event)};
 
                           </div>
                           <div className="flex space-x-2">
